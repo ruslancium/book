87c69e7e4650d457ae03daba7610b8e10ad5b064
ruslan1979 <ruslan.arifullin@gmail.com>
2017-04-09 17:59:27 +0300

    надо переделать многопточность

--- Books/src/name/ruslan/proxy/ConnectionPool.java
+++ Books/src/name/ruslan/proxy/ConnectionPool.java
@@ -10,40 +10,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-public class ConnectionPool<T extends Connection> {
-	private ArrayBlockingQueue<T> connectionQueue;
-	private int poolSize;
+public class ConnectionPool<T extends ProxyConnection> {
 	
-	public ConnectionPool(final int POOL_SIZE, String lookupString) {
-		connectionQueue = new ArrayBlockingQueue<T>(POOL_SIZE);
-		for (int i = 0; i < POOL_SIZE; i++) {
-			//Context ctx = new InitialContext();
-			//DataSource  dataSource = (DataSource) ctx.lookup(lookupString);
-			//T connection = (T) dataSource.getConnection();
-			//все написать в драйверменеджере
-			connectionQueue.offer(connection);			
-		}
-		
-		this.poolSize = POOL_SIZE;
-	}
-   
-	public T getConnection() throws InterruptedException {
-		T connection = null;
-		connection = connectionQueue.take();
-		return connection;		
-	}
-	
-	public void returnConnection(T connection) {
-		connectionQueue.offer(connection); 
-             // возможны утечки соединений
-	}
-	
-	public void releaseConnections() throws InterruptedException, SQLException {
-		for (int i = poolSize; i == 0; i--) {
-			Connection connection = connectionQueue.take();
-			connection.close();
-		}
-	}
-	//more methods 
 }
 
